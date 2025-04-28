999e3dc8d5585c1fe27fd6ed83af2605ac4c42e2
Suhail Bin Abdullah <suhailbinabdullah@gmail.com>
2025-04-28 14:51:29 +0530

    #2 - minor css class modifications

--- taxonomy-news_category.php
+++ taxonomy-news_category.php
@@ -6,13 +6,18 @@
   $term = get_queried_object();
 ?>
 <div class="dsl-articles-three-grid">
-  <div class="container">
-    <h1 class="news-archive-title">news archive: <?php echo esc_html($term->name); ?></h1>
-  </div>
-</div>
-<div class="container">
-  <div class="articles-post-list-wrap">
-  <?php echo do_shortcode("[orbit_query posts_per_page='12' post_type='news_article' style='grid3' tax_query='news_category:" . $term->slug . "'  pagination='1']"); ?>
-  </div>
+  <div class="container" role="main">
+			<div class="row">
+				<div class="col-sm-12">
+          <h1 class="news-archive-title">news archive: <?php echo esc_html($term->name); ?></h1>
+				</div>
+			</div>
+
+      <div class="row">
+				<div class="col-sm-12">
+          <?php echo do_shortcode("[orbit_query posts_per_page='12' post_type='news_article' style='grid3' tax_query='news_category:" . $term->slug . "'  pagination='1']"); ?>
+				</div>
+			</div>
+	</div>
 </div>
 <?php get_footer(); ?>
